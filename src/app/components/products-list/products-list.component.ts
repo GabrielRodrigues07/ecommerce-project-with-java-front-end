852c74af3b60cbc7623365d13a1b8e341fe519b5
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-06-22 12:05:57 -0300

    feat(S10-#20-06-24) adicionando imagem base de produtos

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -4,7 +4,8 @@ import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
+  templateUrl: './products-list-table.component.html',
+  // templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
