cc39fb791a965f40f0416533e9422def23aa6603
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-06-22 11:01:22 -0300

    feat(S10-#20-06-24) criacao classe e service produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "./services/product.service";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { ProductsListComponent } from './components/products-list/products-list.
     ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
