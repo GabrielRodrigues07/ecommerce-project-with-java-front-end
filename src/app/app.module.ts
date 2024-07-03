059ddd53e66c0fcabe896090890e0005fcde85a2
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-03 08:19:01 -0300

    feat(S12-#03-07-24) definindo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import {Routes, RouterModule} from "@angular/router";
 
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductsListComponent},
+  {path: 'category', component: ProductsListComponent},
+  {path: 'products', component: ProductsListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+  ];
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
