21856ba02d1ff5aeb7391de0da9289354f358d06
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-04 19:11:03 -0300

    feat(S12-#03-07-24) implementado rotas para endpoint de categoria

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list-grid.component.html',
-  // templateUrl: './products-list-table.component.html',
-  // templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
 
   products: Product[] = [];
-  constructor(private productService: ProductService) { }
+  currentCategoryId: number = 1;
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(value => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
