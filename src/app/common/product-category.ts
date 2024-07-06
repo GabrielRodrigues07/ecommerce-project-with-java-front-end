46288583d89d3897762b12081a0ee9d2f3a23871
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-06 16:53:01 -0300

    feat(S13-#06-07-24) Criacao dde menu dinamico

--- src/app/common/product-category.ts
+++ src/app/common/product-category.ts
@@ -1,23 +1,6 @@
 export class ProductCategory {
 
-  constructor(public _id: number,
-              public _categoryName: string) {
-  }
-
-
-  get id(): number {
-    return this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get categoryName(): string {
-    return this._categoryName;
-  }
-
-  set categoryName(value: string) {
-    this._categoryName = value;
+  constructor(public id: number,
+              public categoryName: string) {
   }
 }
