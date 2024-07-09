5e9988b82952aa3ef17fdb42dd22696b50161331
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-08 21:43:40 -0300

    feat(S14-#08-07-24) criando novo component e adicionando rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {Routes, RouterModule} from "@angular/router";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes: Routes = [
+  {path: 'search/:keyword', component: ProductsListComponent},
   {path: 'category/:id', component: ProductsListComponent},
   {path: 'category', component: ProductsListComponent},
   {path: 'products', component: ProductsListComponent},
@@ -20,7 +22,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
