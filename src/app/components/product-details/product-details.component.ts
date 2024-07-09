d477749986204107aad2d48ebd1b27795879fac3
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-09 11:17:30 -0300

    feat(S15-#09-07-24) mostrando nome do produto na tela a partir de um id

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../../common/product";
+import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+     this.handleProductDetails();
+    });
   }
 
+  handleProductDetails() {
+    //get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    );
+  }
 }
