a7bc2758ff314a1a81815df3019cdc344d83dfcf
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-10 08:27:47 -0300

    feat(S16-#10-07-24) instalando modulo do bootstrap para paginacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductsListComponent } from './components/products-list/products-list.component';
+import {AppComponent} from './app.component';
+import {ProductsListComponent} from './components/products-list/products-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {Routes, RouterModule} from "@angular/router";
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
+import {SearchComponent} from './components/search/search.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 const routes: Routes = [
@@ -19,7 +21,8 @@ const routes: Routes = [
   {path: 'products', component: ProductsListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', redirectTo: '/products', pathMatch: 'full'},
-  ];
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,9 +34,11 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
