468af6b6b82e0114f8ec3fc99d1cdfc63af230cc
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-17 08:19:16 -0300

    feat(S16-#17-07-24) possibilitando usuario a escolher o tamanho da pagina

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -16,7 +16,7 @@ export class ProductsListComponent implements OnInit {
   searchMode: boolean = false;
 
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -77,4 +77,10 @@ export class ProductsListComponent implements OnInit {
       }
     );
   };
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
