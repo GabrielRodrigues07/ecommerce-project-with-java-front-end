cbee577245a393f3f153cec65862f286464eb8f2
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-22 08:54:11 -0300

    refactor(S17-#22-07-24) melhorando codigo

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,12 +24,7 @@ export class CartService {
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
 
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = theCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find( theCartItem => theCartItem.id === theCartItem.id )!;
       //check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
     }
