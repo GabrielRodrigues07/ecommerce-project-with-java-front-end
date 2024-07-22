e4b17af483ee8ab6338313bc828d1726f7b192c3
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-22 09:04:09 -0300

    feat(S17-#22-07-24) Adicionando cart na tela de detalhes

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../../common/product";
 import {ProductService} from "../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
+import {CartService} from "../../services/cart.service";
+import {CartItem} from "../../common/cart-item";
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product!: Product;
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -29,5 +32,12 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     );
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
