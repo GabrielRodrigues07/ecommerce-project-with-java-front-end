cf70aa67eea9330f8421e00d0561ad177092502f
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-22 20:11:59 -0300

    feat(S18-#22-07-24) listando produtos na tela de detalhes do carrinho

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
   }
 
+  listCartDetails() {
+    // get a handle to the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    // compute cart total price and quantity
+    this.cartService.computerCartTotals();
+  }
 }
