c9d29956f54ee17d8e4dffc1c40a95644b128ecf
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-22 21:26:45 -0300

    feat(S18-#22-07-24) implementado decremento e remove de todos os itens

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -71,4 +71,27 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('________')
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computerCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computerCartTotals();
+    }
+  }
 }
