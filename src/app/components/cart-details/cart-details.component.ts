c54bb2e159a4466a697430c1d804b1cc5c0bba7d
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-07-22 21:26:45 -0300

    feat(S18-#22-07-24) implementado decremento e remove de todos os itens

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -40,4 +40,12 @@ export class CartDetailsComponent implements OnInit {
   incrementQuantity(tempCartItem: CartItem) {
     this.cartService.addToCart(tempCartItem);
   }
+
+  decrementQuantity(tempCartItem: CartItem) {
+    this.cartService.decrementQuantity(tempCartItem);
+  }
+
+  remove(tempCartItem: CartItem) {
+    this.cartService.remove(tempCartItem);
+  }
 }
