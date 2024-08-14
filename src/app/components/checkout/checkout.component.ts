cd0cbe13005fc56ba045d4342d6e4143866f6d3f
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-08-14 19:33:23 -0300

    feat(S21-#13-08-24) Adicionado validação para billingAddress

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -74,11 +74,23 @@ export class CheckoutComponent implements OnInit {
       }),
 
       billingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: new FormControl('', [Validators.required]),
+        street: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Luv2ShopValidators.notOnlyWhitespace
+        ]),
+        city: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Luv2ShopValidators.notOnlyWhitespace
+        ]),
+        state: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Luv2ShopValidators.notOnlyWhitespace
+        ])
       }),
 
       creditCard: this.formBuilder.group({
@@ -226,6 +238,26 @@ export class CheckoutComponent implements OnInit {
 
   get shippingAddressCountry() {
     return this.checkoutFormGroup.get('shippingAddress.country');
+  }
+
+  get billingAddressStreet() {
+    return this.checkoutFormGroup.get('billingAddress.street');
+  }
+
+  get billingAddressCity() {
+    return this.checkoutFormGroup.get('billingAddress.city');
+  }
+
+  get billingAddressState() {
+    return this.checkoutFormGroup.get('billingAddress.state');
+  }
+
+  get billingAddressZipCode() {
+    return this.checkoutFormGroup.get('billingAddress.zipCode');
+  }
+
+  get billingAddressCountry() {
+    return this.checkoutFormGroup.get('billingAddress.country');
   }
 
 }
